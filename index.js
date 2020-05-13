fcbf2f71f5c0478fea49051eca5adee2f965f129
austinckohler <austinckohler@gmail.com>
2020-05-12 19:20:16 -0600

    added event listener for form data

--- index.js
+++ index.js
@@ -4,10 +4,47 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content loaded')
 
    function fetchCities() {
-       
-   } fetch('http://localhost:3000/cities')
+    fetch('http://localhost:3000/cities')
     .then(response => response.json())
     .then(cities => console.log(cities))
+   }
+    
+   fetchCities()
 
-    fetchCities()
+    const newCityForm = document.querySelector('#add-city')
+
+    newCityForm.addEventListener('submit', event => {
+        event.preventDefault()
+        const formData = new FormData(newCityForm)
+        const name = formData.get('name')
+        const population = formData.get('population')
+        const state = formData.get('state')
+        fetch('http://localhost:3000/cities', {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                city: {
+                name: name,
+                population: population,
+                state: state
+                }
+            })
+        })
+    })
+    
+
+
+// function displayCities() {
+//     cities.forEach(city => {
+//         const h1 = document.createElement("h1")
+
+//         h1.innerText = city.name
+
+//         document.body.append(h1)
+//     });
+    
+// }
 })
